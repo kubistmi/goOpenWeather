2f62c86ce2c3968525dfa7af203891a8d0a1516b
Michal Kubista <kubistmi@gmail.com>
2019-08-01 08:43:09 +0200

    BugFIx: Interactive tty doesn't make sense.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ container=$(docker ps | grep psql_weather | awk '{print $1}')
 
 /home/kubistmi/go/src/weather/weather
 
-docker exec -it $container /bin/bash -c 'pg_dump weather > /var/lib/postgresql/11/data/weather_$(date -d +2hours +"%Y-%m-%d-%H").sql'
+docker exec $container /bin/bash -c 'pg_dump weather > /var/lib/postgresql/11/data/weather_$(date -d +2hours +"%Y-%m-%d-%H").sql'
