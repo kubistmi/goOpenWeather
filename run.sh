76677e54c951ba7091d2edfd2bfc62a68e456712
kubistmi <kubistmi@gmail.com>
2019-08-11 15:21:31 +0100

    Clear old backups.

--- run.sh
+++ run.sh
@@ -4,3 +4,7 @@ container=$(docker ps | grep psql_weather | awk '{print $1}')
 /home/kubistmi/go/src/weather/weather
 
 docker exec $container /bin/bash -c 'pg_dump weather > /var/lib/postgresql/11/data/weather_$(date -d +2hours +"%Y-%m-%d-%H").sql'
+
+docker exec $container /bin/bash -c 'if [ "$(ls /var/lib/postgresql/11/data/ -1 | wc -l)" -gt 1 ];
+then echo "$(ls /var/lib/postgresql/11/data/ -t | tail -1)";
+fi;'
